576e1cea03fb6f41f22e89d6394f22a7b84d5640
OlafVrijmoet <60693445+OlafVrijmoet@users.noreply.github.com>
2020-06-03 14:17:37 +0200

    Da battery class
    Two different variables for x & y axis

--- Battery.py
+++ Battery.py
@@ -14,7 +14,9 @@ class Battery():
     def __init__(self, float x_axis, float y_axis, float capacity):
 
         # saves battery location on grid
-        self.position = [x_axis, y_axis]
+        self.position_x = x_axis
+        
+        self.position_y = y_axis
 
         # saves battery capacity
         self.capacity = capacity
