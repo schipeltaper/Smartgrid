8b80ae65dbbe2a4f0df9333c1325cb9844780d6f
Kuburat Idinokhai <Kuburat@MacBook-Air-van-Kuburat.local>
2020-06-03 15:35:31 +0200

    calculate total costs (for all batteries)

--- Algorithms.py
+++ Algorithms.py
@@ -16,3 +16,10 @@ def sortHouse(houses):
         houses[grootste_locatie] = temp
 
     return houses
+    
+# calculate costs for list of batteries
+def totalCosts(batteries):
+    total_costs = 0
+    for all battery in batteries:
+        total_costs += battery.costs
+    return total_costs
