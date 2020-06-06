51e39c77e603ec58295b2b03dc375f343ac1681d
OlafVrijmoet <ovrijmoet96@gmail.com>
2020-06-06 15:40:19 +0200

    added remove and empy funtion to battery class

--- assignment1/test.py
+++ assignment1/test.py
@@ -2,14 +2,20 @@ from House import *
 from Algorithms import *
 from configuration import *
 from greedy_algorithm import *
-from Randomworld import *
 
 def main():
 
     greedy_solution = Greedy()
     greedy_solution.adding_houses()
 
-    random = Randomworld_1()
+    # print amout of items in battery 1
+    print(len(greedy_solution.batteries[0].houses_in_battery))
+
+    # removes second house from first battery
+    greedy_solution.batteries[0].remove_house(greedy_solution.batteries[0].houses_in_battery[1])
+    
+    # print amout of items in battery 1 after item removed
+    print(len(greedy_solution.batteries[0].houses_in_battery))
 
     print(greedy_solution.batteries[0].houses_in_battery[1].production)
     print(greedy_solution.batteries[0].houses_in_battery[1].production)
@@ -19,7 +25,6 @@ def main():
     print(greedy_solution.batteries[2].houses_in_battery[0].production)
 
     print(totalCosts(greedy_solution.batteries))
-    print(random.costs)
 
     # house1 = House(1, 2, 3)
     # house2 = House(1, 3, 2)
