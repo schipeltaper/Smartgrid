9075f1bd16cd9fc56216e6d6a97d49e052a4c6c8
OlafVrijmoet <ovrijmoet96@gmail.com>
2020-06-08 17:42:46 +0200

    sorting houses by proximity & sorting houses from low to high production algorythms added

--- assignment1/greedy_algorithm.py
+++ assignment1/greedy_algorithm.py
@@ -7,9 +7,18 @@ class Greedy():
     # initialises a battery with a location and capacity
     def __init__(self):
 
+        # place to store batteries
         self.batteries = []
 
-    # sorting houses
+        # adding batteries
+        self.adding_batteries()
+
+        # temp location to store list of all houses -- specificly for district_1 for now
+        self.all_houses = district_1["houses"]
+        self.connected_houses = []
+
+
+    # sorting houses hight too low
     def sort_house(self, houses):
         
         # Selection Sort
@@ -26,19 +35,72 @@ class Greedy():
             houses[biggest_location] = self.temp
 
         return houses
+    
+    # sorting houses low too high
+    def house_low_sort(self, houses):
 
+        # Selection Sort
+        for self.house_numb in range(len(houses)):
+
+            # We're searching for the highest production
+            for self.house_numb2 in range(self.house_numb, len(houses)):
+                if houses[self.house_numb2].production < houses[self.house_numb].production:
+                    self.temp = houses[self.house_numb]
+
+                    houses[self.house_numb] = houses[self.house_numb2]
+
+                    houses[self.house_numb2] = self.temp
+
+        return houses
+    
+    # sorting batteries by closest battery
+    def proximity_sort(self):
+
+        self.house_low_sort(self.all_houses)
+
+        self.closses_batteries = self.batteries.copy()
+        
+        # itterate through all houses
+        for self.the_house in self.all_houses:
+
+            # itterate through batteries
+            for self.battery_numb in range(len(self.closses_batteries)):
+
+                # itterate through remaining batteries
+                for self.battery_numb2 in range(self.battery_numb, len(self.closses_batteries)):
+                    
+                    # check if contested battery is closses to other battery further down
+                    if self.closses_batteries[self.battery_numb2].cable_length(self.the_house) < self.closses_batteries[self.battery_numb].cable_length(self.the_house):
+                        
+                        # switch batteries in list
+                        self.temp_save = self.closses_batteries[self.battery_numb]
+
+                        self.closses_batteries[self.battery_numb] = self.closses_batteries[self.battery_numb2]
+
+                        self.closses_batteries[self.battery_numb2] = self.temp_save
+            
+            # add house to closest none full house
+            for self.the_battery in self.closses_batteries:
+
+                # make sure battery is not full
+                if self.the_battery.battery_full(self.the_house) is not True:
+                    
+                    # add house to battery
+                    self.the_battery.add_house(self.the_house)
+                    
+                    # go to next house
+                    break
+    
+    
     # adds batteries too battiers in Greedy object
     def adding_batteries(self):
         self.batteries = copy.deepcopy(district_1["batteries"])
     
     # function to add houses to batteries
     def adding_houses(self):
-        
-        # temp location to store list of all houses -- specificly for district_1 for now
-        self.all_houses = self.sort_house(district_1["houses"])
-        self.connected_houses = []
 
-        self.adding_batteries()
+        # sorting houses
+        self.sort_house(self.all_houses)
 
         # battery currently storing houses in
         self.battery_numb = 0
