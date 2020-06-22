2dca03d162bd12b945054bfd2ac1537863e153a1
Kuburat Idinokhai <Kuburat@MacBook-Air-van-Kuburat.local>
2020-06-22 21:42:17 +0200

    interface start

--- assignment1/__main__.py
+++ assignment1/__main__.py
@@ -15,13 +15,19 @@ def totalCosts(batteries):
     return total_costs
 
 def main():
-
+    '''
+    interface to run algorithms that minimize the cost in the Smartgrid case.
+    '''
+    
+    # collect user input
     district_id = input("Choose district (1,2 or 3):")
     cable_rule = input("Choose cable rule: \n 1. own-costs\n 2. shared costs\n")
     if cable_rule == 1:
         algorithm_id = input("Choose algorithm: \n 1. Random\n 2. Greedy\n 3. Random hill descent\n \
         4. Random Greedy\n 5. Simulated Annealing\n 6. Hill Climber\n")
-
-
+    if cable_rule == 2:
+        algorithm_id = input("Choose algorithm: \n 1. Random\n 2. Optimum Deluxe\n") + 6
+    # run algorithm
+    
 if __name__ == "__main__":
     main()
